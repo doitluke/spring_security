5955522f7b50dc9c6e4767b0b05f2a87af9257e6
Ho <doitluke@naver.com>
2020-09-16 01:38:14 +0900

    초기화

---
+++ src/main/java/me/luke/demospringsecurityform/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package me.luke.demospringsecurityform.config;
+
+import me.luke.demospringsecurityform.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AccountService accountService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http.formLogin();
+
+        http.httpBasic();
+    }
+
+}
